4deb4751493a433d58e3557937b222e66ea53f7d
Erick Samuel Guerrero <56603352+eguerrero-lumston@users.noreply.github.com>
2021-02-02 15:05:02 -0600

    Initial Commit

---
+++ RedditPosts/RedditPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditPostsApp.swift
+//  RedditPosts
+//
+//  Created by Erick Guerrero on 02/02/21.
+//
+
+import SwiftUI
+
+@main
+struct RedditPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
